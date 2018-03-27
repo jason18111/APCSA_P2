d701b4fa1212b294b0dbb0242ea17ac93da17acf
jasonlin18111 <36010566+jasonlin18111@users.noreply.github.com>
2018-03-27 01:27:53 -0700

    Update Lab24d.java

--- Unit14/Unit14_2016/Unit14-Assignments/Lab24d.java
+++ Unit14/Unit14_2016/Unit14-Assignments/Lab24d.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
@@ -14,14 +14,12 @@ public class Lab24d
 	public static void main( String args[] ) throws IOException
 	{
 		Scanner file = new Scanner (new File("Lab24d.dat"));
-
-
-
-
-
-
-
-
+		int size = file.nextInt();
+		file.nextLine();
+		for(int i = 0; i<size; i++) {
+			TicTacToe thing = new TicTacToe(file.nextLine());
+			System.out.println(thing);
+		}
 	}
 }
 
